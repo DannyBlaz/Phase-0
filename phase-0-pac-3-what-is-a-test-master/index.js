bd05b9283b76c28afce319eff4cd184826c61d17
Daniel <dannyblaze469@gmail.com>
2021-06-21 13:28:55 -0400

    Complete My First Software Engineering Assignment

--- phase-0-pac-3-what-is-a-test-master/index.js
+++ phase-0-pac-3-what-is-a-test-master/index.js
@@ -1,6 +1,12 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// const name = "Joe";
+// const height = 74;
+// const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
-console.log(message);
+// module.exports = {name, height, message};
+
+// const num = [1 + 2 + "puppies" + 4 + 5];
+// console.log("joey", num);
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log('I was called!');
+}
